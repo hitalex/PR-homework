3c22830282b4def6cb5f81cedbf41b4b4477a1df
hitalex@laptop <kqingqingcaho@gmail.com>
2013-10-21 23:55:09 +0800

    done with knn

--- hw1/knn.py
+++ hw1/knn.py
@@ -1,5 +1,7 @@
 #coding=utf8
 
+from random import choice
+
 import sklearn.metrics
 
 import readdata
@@ -10,7 +12,29 @@ def knn_predict(num_class, x_train, y_train, x_test, k):
     @k: hyper-parameter
     """
     y_pred = []
-    
+    for x in x_test:
+        # (distance, classlabel)
+        knn_neighbor =  [(flot('inf'), -1)] * k
+        for t, l in x_train, y_train:
+            dis = sum((x - t)**2)
+            # insert to knn neighbors
+            i = 0
+            while i<k:
+                if knn_neighbor[i][0] > dis:
+                    knn_neighbor.insert(i, (dis, l))
+                    knn_neighbor.pop() # remove the last item
+                    break
+                i = i + 1
+        # find votes
+        votes = [0] * num_class
+        for i in range(k):
+            index = knn_neighbor[i][1]
+            votes[index] = votes[index] + 1
+        # find the dominating label in knn neighbor
+        prediction = votes.index(max(votes))
+        prediction = choice(prediction)
+        
+        y_pred.append(prediction)
     
 if __name__ == '__main__':
     import sys
