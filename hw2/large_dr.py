a6f180cc00d0cbd00642b12a85ec1e1b2eae6605
Qingchao Kong <kqingchao@gmail.com>
2013-11-07 21:22:54 +0800

    try some other equivalent expressions and it works, still donnot know why

--- hw2/large_dr.py
+++ hw2/large_dr.py
@@ -5,7 +5,7 @@ HWDB1.1 large database PCA and Fisher LDA dimension reduction
 Requirement: Just one scan through the database
 """
 import resource
-import gc
+from datetime import datetime
 
 import numpy as np
 
@@ -18,17 +18,17 @@ def main():
     
     d = 512 # number of features
     
-    mean = np.matrix([0] * d).T # mean for all dataset
+    mean = np.array([0] * d) # mean for all dataset
     moment = np.zeros((d,d), np.float64) #  second order origin moment for all dataset
     
     class_mean = []
     
-    total = 897758 # total number of samples
+    total = 0 # total number of samples
     class_total = [] # total number of samples for each class
     
-    total_inverse = 1.0 / total
+    x = np.array([0] * d)
+    pre_time = datetime.now()
     
-    x = np.matrix([0] * d).T
     for i in range(1, 241):
         # scan through the whole database
         path = train_path + str(1000+i) + '.txt'
@@ -37,7 +37,7 @@ def main():
         print 'Current mem usage: %s KB.' % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
         
         for line in f:
-            #total = total + 1
+            total = total + 1
             line = line.strip()
             seg_list = line.split(' ')
             char = seg_list[0]
@@ -47,31 +47,35 @@ def main():
                 char_map[char] = char_class
                 
                 char_list.append(char)
-                class_mean.append(np.array([0] * d).T)
+                class_mean.append(np.array([0] * d))
                 
                 class_total.append(0)
-                
-                #print 'Add new char: %s with index: %d' % (char, char_class)
             else:
                 char_class = char_map[char]
                 
             class_total[char_class] = class_total[char_class] + 1
             
             for i in range(0, d):
-                x[i, 0] = int(seg_list[i+1])
+                x[i] = int(seg_list[i+1])
             
             mean = mean + x
-            moment = moment + (x * x.T)
-            
             class_mean[char_class] = class_mean[char_class] + x
             
-            #print 'Current mem usage: %s KB.' % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+            tmp = np.matrix(x).T
+            moment = moment + (tmp * tmp.T)
             
+            #print 'Current mem usage: %s KB.' % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss            
             
         f.close()
+        
+        now = datetime.now()
+        delta = now - pre_time
+        pre_time = now
+        print 'Takes: %d secs.\n' % delta.total_seconds()
     
     class_count = len(char_list) #number of classes
     
+    mean = np.matrix(mean).T
     mean = 1.0 / total * mean
     cov = 1.0 / total * moment - mean * mean.T
     #cov = moment - mean * mean.T
@@ -83,6 +87,7 @@ def main():
         Ni = class_total[i]
         class_prior[i] = Ni * 1.0 / total
         class_mean[i] = 1.0 / Ni * class_mean[i]
+        class_mean[i] = np.matrix(class_mean[i]).T
         
         tmp = class_mean[i] - mean
         Sb = Sb + class_prior[i] * (tmp * tmp.T)
@@ -91,10 +96,18 @@ def main():
     Sw = St - Sb
     
     # 序列化已经计算完成的对象
+    print 'Dumping into file...'
     import pickle
     f = open('matrixpickle', 'w')
     pickle.dump([cov, Sw, Sb], f)
     f.close()
-
+    
+    print 'Saving char list...'
+    f = open('charlist.txt', 'w')
+    for char in char_list:
+        f.write(char + '\n')
+    f.close()
+        
 if __name__ == '__main__':
     main()
+    
