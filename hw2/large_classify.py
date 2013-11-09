fix formula bugs

hw2/large_classify.py
@@ -15,28 +15,33 @@ def classify_LDF(num_class, num_feature, class_prior, class_mean, avg_cov, W, te
     """
     d = num_feature
     
-    avg_cov_inv = avg_cov.getI()
+    Wt = W.T
+    new_avg_cov = Wt * avg_cov * W
+    avg_cov_inv = new_avg_cov.getI()
     
     weight = [0] * num_class
     weight0 = [0] * num_class
-    Wt = W.T
+    
     for i in range(num_class):
         weight[i] = 2 * avg_cov_inv.T * Wt * class_mean[i]
-        weight0[i] = 2 * math.log(class_prior[i]) * class_mean[i].T * W * avg_cov_inv * Wt * class_mean[i]
+        weight0[i] = 2 * math.log(class_prior[i]) - class_mean[i].T * W * avg_cov_inv * Wt * class_mean[i]
         
     f = open(testpath, 'r')
     y_true = []
     y_pred = []
     
     correct = 0
+    tmp = np.matrix([0] * d).T
+    
     for line in f:
         line = line.strip()
-        seg_list = line.split()
+        seg_list = line.split(',')
         
-        x = [0] * d
         for i in range(d):
-            x[i] = int(seg_list[i])
-        x = np.matrix(x).T
+            tmp[i, 0] = int(seg_list[i])
+        
+        x = Wt * tmp # transform to low dim
+        
         label = int(seg_list[-1])
         y_true.append(label)
         
@@ -50,8 +55,11 @@ def classify_LDF(num_class, num_feature, class_prior, class_mean, avg_cov, W, te
                 
         y_pred.append(prediction)
         
+        print 'Label: %d, prediction: %d' % (label, prediction)
+        
         if label == prediction:
             correct = correct + 1
+            print 'Correct: ', correct
         
     f.close()
     total = len(y_pred)
@@ -106,21 +114,32 @@ def classify_nearest_mean(num_class, num_feature, class_mean, W, testpath):
 def main():
     # laod pre-computed variabls
     
+    print 'Loading variables from pickle...'
     f = open('matrixpickle', 'r')
     total, char_list, class_count, class_total, mean, cov, class_mean, \
-        class_prior, Sb = pickle.load(f) # version 2
-
+        class_prior, Sb = pickle.load(f)
+    f.close()
+    
+    char_map = dict()
+    index = 0
+    for char in char_list:
+        char_map[char] = index
+        index = index + 1
+    
     d = 512
     dr_method = 'PCA'
     #dr_method = 'FisherLDA'
     
-    for s in range(10, d, 10):
+    for s in [20]:
+        print 'Dimension reduction: %d' % s
+        
+        St = cov
+        Sw = St - Sb
+        
         if dr_method == 'PCA':
-            eig_values, eig_vectors = linalg.eig(cov_matrix)
+            eig_values, eig_vectors = np.linalg.eig(cov)
             
-        else if dr_method == 'FisherLDA':
-            St = cov
-            Sw = St - Sb
+        elif dr_method == 'FisherLDA':
             eig_values, eig_vectors = scipy.linalg.eigh(Sb, Sw)
         else:
             print 'Unknown dimention reduction method!'
@@ -136,10 +155,12 @@ def main():
         
         testpath = '/home/kqc/dataset/HWDB1.1/test.txt'
         print 'LDF prediction:'
-        y_true, y_pred = classify_LDF(num_class, num_feature, class_prior, class_mean, avg_cov, W, testpath)
+        y_true, y_pred = classify_LDF(class_count, d, class_prior, class_mean, Sw, W, testpath)
+        
+        return 
         
         print 'Nearest mean prediction:'
-        y_true, y_pred = classify_nearest_mean(num_class, num_feature, class_mean, W, testpath)
+        y_true, y_pred = classify_nearest_mean(class_count, d, class_mean, W, testpath)
         
         
 if __name__ == '__main__':