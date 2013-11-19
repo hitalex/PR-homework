c4a7fa6f8e74b5eda289c42aad3ae8e31f69cbbc
Qingchao Kong <kqingchao@gmail.com>
2013-11-19 20:59:12 +0800

    refactor the RBF training

--- hw3/RBF_train.py
+++ hw3/RBF_train.py
@@ -6,43 +6,46 @@ RBF network training
 import pickle
 from math import exp
 import os
+import random
+import sys
+from select import select
 
 import numpy as np
 import numpy.linalg
 import numpy.random
+import ipdb
 
 os.sys.path.append('/home/kqc/github/PR-homework/')
 from hw1.readdata import read_dataset
 
-def get_initial_weights():
-    """ Set the initial weights and the shape is (num_class, num_hidden+1)
-    weights[:, 0] are biases
+def get_initial_weight():
+    """ Set the initial weight and the shape is (num_class, num_hidden+1)
+    weight[:, 0] are biases
     Note: set them to small random values chosen from a zero-mean Gaussian with a standard deviation of about 0.01
     """
-    weights = np.zeros((num_class, num_hidden+1)) # add biases
+    weight = np.zeros((num_class, num_hidden+1)) # add biases
     
-    weights[:, :-1] = np.random.normal(0, 0.01, size=(num_class, num_hidden))
-    weights[:, -1] = 0 # set biases to 0
+    weight[:, :-1] = np.random.normal(0, 0.01, size=(num_class, num_hidden))
+    weight[:, -1] = 0 # set biases to 0
     
-    return weights
+    return weight
     
 def get_hidden_layer_parameter():
-    """ Set the hidden layer prameters: means and \sigma^2
+    """ Set the hidden layer prameters: mean and \sigma^2
     """
-    means = np.zeros((num_feature, num_hidden)) # shape: num_feature * num_hidden
-    sigma2 = np.array([0] * num_hidden) # \sigma^2
+    mean = np.zeros((num_feature, num_hidden)) # shape: num_feature * num_hidden
 
     # all \sigma are the same
-    sigma2 = [1] * num_hidden
+    sigma2 = np.array([1000] * num_hidden)
     # get the mean of the whole training dataset
     m = np.mean(x_train, axis=0)
     # the covariance matrix
     # assuming the features are independent
-    cov = 0.5 * np.eye(num_feature)
-    means = np.random.multivariate_normal(m, cov, size=num_hidden) # shape: num_hidden * num_feature
-    means = means.T
+    cov = 0.05 * np.eye(num_feature)
+    mean = np.random.multivariate_normal(m, cov, size=num_hidden) # shape: num_hidden * num_feature
+    mean = mean.T
     
-    return means, sigma2
+    return mean, sigma2
     
 def sigmoid(x):
     """ the Sigmoid function
@@ -50,7 +53,7 @@ def sigmoid(x):
     """
     return 1.0 / (1 + exp(-x))
     
-def get_output(means, sigma2, weights, x):
+def get_output(mean, sigma2, weight, x):
     """ Get the output for the single input instance, i.e. y_k(x_i)
     Return: the probability of x belonging to each class, list of length num_class
     """
@@ -58,55 +61,84 @@ def get_output(means, sigma2, weights, x):
     u[-1] = 1    # weight of the biase
     
     for j in range(0, num_hidden):
-        u[j] = exp(-1 * numpy.linalg.norm(x - means[:, j])**2 / (2 * sigma2[j]))
+        u[j] = exp(-1 * numpy.linalg.norm(x - mean[:, j])**2 / (2 * sigma2[j]))
         
     v = [0] * num_class
     y = [0] * num_class
     for k in range(num_class):
         v[k] = 0
         for j in range(num_hidden+1):
-            v[k] += (weights[k, j] * u[j])
+            v[k] += (weight[k, j] * u[j])
         
         y[k] = sigmoid(v[k])
         
-    return y, u
+    return y, v, u
     
-def update_weight(weights, target, y, u, learning_rate):
-    """ Update network weights
-    Return: the updated weights and delta
-    Note: This update routine changes the original weights
-    """
+def get_delta(y, target):
     delta = [0] * num_class
     for k in range(num_class):
         delta[k] = (y[k] - target[k]) * y[k] * (1 - y[k])
-        for j in range(num_hidden+1):
-            gradient = delta[k] * u[j]
-            weights[k, j] -= (learning_rate * gradient)
-            
+    
     return delta
-
-def update_gaussian_parameter(means, sigma2, weights, x, target, output, u, delta, learning_rate):
-    """ Update Gaussian parameters using the old weights, instead of the updated weights
-    Return: the updated means and sigma2
+    
+def compute_weight_gradient(delta, u):
+    """ Compute weight gradient
+    Return: 
+    Note: This update routine changes the original weight
     """
-    # update the means
-    weights_delta_coff = [0] * num_hidden
+    delta = np.matrix(delta).T # shape: num_class * 1
+    u = np.matrix(u) # shape: 1 * (num_hidden+1)
+    
+    gradient = delta * u
+    
+    return np.array(gradient)
+
+def compute_gaussian_gradient(mean, sigma2, weight, x, delta, u):
+    """ Compute the gradient of Gaussian parameters using the old weight, instead of the updated weight
+    Return: the gradient
+    """
+    # update the mean
+    weight_delta_coff = [0] * num_hidden
+    gradient_mean = np.zeros(mean.shape)        # shape: num_feature * num_hidden
+    gradient_sigma2 = np.zeros(sigma2.shape)
     
     for j in range(num_hidden):
         # update each mean
-        weights_delta_coff[j] = np.dot(weights[:, j], delta)
+        weight_delta_coff[j] = np.dot(weight[:, j], delta)
         
-        pgradient_mean = u[j] / (2 * sigma2[j]) * (x - means[:, j]) # shape: num_feature * 1
-        gradient_mean =  weights_delta_coff[j] * pgradient_mean
+        pgradient_mean = 1.0 * u[j] / (2 * sigma2[j]) * (x - mean[:, j]) # shape: num_feature * 1
+        gradient_mean[:, j] =  weight_delta_coff[j] * pgradient_mean
         
-        pgradient_sigma2 = u[j] / (2 * sigma2[j]) * np.linalg.norm(x-means[:, j])**2
-        gradient_sigma2 = weights_delta_coff[j] * pgradient_sigma2
+        pgradient_sigma2 = 1.0 * u[j] / (2 * sigma2[j]) * np.linalg.norm(x-mean[:, j])**2
+        gradient_sigma2[j] = weight_delta_coff[j] * pgradient_sigma2
         
-        # update means and sigma together
-        means[:, j] -= (learning_rate * gradient_mean)
-        sigma2[j] -= (learning_rate * gradient_sigma2)
+    return gradient_mean, gradient_sigma2
 
-def check_convergence(previous_weights, weights):
+def classify_validation_set(mean, sigma2, weight):
+    """ Method: check the classification acc of the validation set
+    """
+    total = len(x_validation)
+    correct = 0
+    for i in range(total):
+        x = x_validation[i]
+        y_true = y_validation[i]
+        
+        output, v, u = get_output(mean, sigma2, weight, x)
+        maxp = 0
+        y_pred = -1
+        for k in range(num_class):
+            if output[k] > maxp:
+                maxp = output[k]
+                y_pred = k
+        
+        if y_true == y_pred:
+            correct += 1
+            
+    print 'Classification report on validation set: Total=%d, Correct=%d, Acc=%f' % (total, correct, correct*1.0/total)
+    
+    return correct*1.0/total
+    
+def check_convergence(val_acc):
     """ Check if the results has converged
     """
     return False
@@ -115,14 +147,15 @@ def train_RBF(learning_rate, max_iter, is_gaussian_unknown = False):
     """ Train RBF network: assume m_j and sigma_j^2 are known for j=1..num_hidden
     max_iter: max iterations
     learning_rate: learning rate
-    is_gaussian_unknown: if True, assume means and sigma^2 are unknown; if False, assume they are known
+    is_gaussian_unknown: if True, assume mean and sigma^2 are unknown; if False, assume they are known
     
     Note: Consider how to set learning rate for different parameters for different iterations
     """
-    means, sigma2 = get_hidden_layer_parameter()
-    weights = get_initial_weights()
+    mean, sigma2 = get_hidden_layer_parameter()
+    weight = get_initial_weight()
     
     train_count = len(x_train)
+    val_acc = [-1] * max_iter    # validation set acc list
     for t in range(max_iter):
         print 'Iteration: ', t+1
         for i in range(train_count):
@@ -132,19 +165,30 @@ def train_RBF(learning_rate, max_iter, is_gaussian_unknown = False):
             target = [0] * num_class
             target[y] = 1
             # get the output given an input
-            output, u = get_output(means, sigma2, weights, x)
-            previous_weights = np.array(weights) # weights befor updating
-            # update weights using the gradient descent algorithm
-            delta = update_weight(weights, target, output, u, learning_rate)
+            output, v, u = get_output(mean, sigma2, weight, x)
+            delta = get_delta(output, target)
+
+            # update weight using the gradient descent algorithm
+            gradient_weight = compute_weight_gradient(delta, u)
             
             if is_gaussian_unknown:
-                update_gaussian_parameter(means, sigma2, previous_weights, x, target, output, u, delta, learning_rate)
+                gradient_mean, gradient_sigma2 = compute_gaussian_gradient(mean, sigma2, weight, x, delta, u)
+                mean -= (learning_rate * gradient_mean)
+                sigma2 -= (learning_rate * gradient_sigma2)
+                
+            # update weight
+            weight -= (learning_rate * gradient_weight)
+        
+        acc = classify_validation_set(mean, sigma2, weight)
+        val_acc[t] = acc
+        
+        if check_convergence(val_acc):
+            print 'Convergence criterion has been met and training is over!'
+            break
+        # waiting for human intervention
+        
             
-            if check_convergence(previous_weights, weights):
-                print 'Convergence criterion has been met and training is over!'
-                break
-            
-    return weights, means, sigma2, t
+    return mean, sigma2, weight, t
             
 def main():
     """
@@ -153,10 +197,16 @@ def main():
         hidden layer: num_hidden
         output layer: num_class
     """
-    learning_rate = 1.0
-    max_iter = 10
+    learning_rate = 50.0
+    max_iter = 100
     # assume m_j and sigma_j^2 are known for j=1..num_hidden
-    weights, means, sigma2, num_iter = train_RBF(learning_rate, max_iter, True)
+    mean, sigma2, weight, num_iter = train_RBF(learning_rate, max_iter, True)
+    
+    print 'Dumping into file...'
+    f = open(dataset_name + '_RBF_pickle', 'w')
+    pickle.dump([dataset_name, num_feature, num_hidden, num_class, mean, sigma2, weight, num_iter], f)
+    #pickle.dump([cov, Sw, Sb], f) # version 1
+    f.close()
 
 if __name__ == '__main__':
     """ Load dataset and start to train the network
@@ -167,5 +217,14 @@ if __name__ == '__main__':
     
     print 'Reading dataset: ', dataset_name
     num_class, num_feature, x_train, y_train, x_test, y_test = read_dataset(dataset_name)
+    
+    # set a validation set to check convergence
+    count = len(x_train) / 3
+    # Note: the dataset has been shuffled
+    x_validation = np.array(x_train[:count])
+    y_validation = np.array(y_train[:count])
+    
+    x_train = x_train[count:]
+    y_train = y_train[count:]
     
     main()
